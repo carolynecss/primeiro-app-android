71ce6a5e3c6254e371e0177fa95d1d6355a2e339
Carol <147120704+carolynecss@users.noreply.github.com>
2025-10-01 18:22:59 -0300

    Add files via upload

---
+++ primeiro-app-android/app/src/main/java/com/rafael/firstapplication_2/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.rafael.firstapplication_2
+
+import android.os.Bundle
+import android.widget.Toast
+
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rafael.firstapplication_2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // val button = findViewById<Button>(R.id.button)
+        // val editText = findViewById<EditText>(R.id.edit_text)
+        val editTextEmail = binding.editText
+        val editTextPassword = binding.editTextPassword // Campo de senha
+        val button = binding.button
+
+
+        button.setOnClickListener {
+            val email = editTextEmail.text.toString() // Pega o texto do email
+            val password = editTextPassword.text.toString() // Pega o texto da senha
+
+            // Exibe um Toast com os dados inseridos
+            Toast.makeText(
+                this,
+                "Email: $email\nSenha: $password",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+    }
+}
